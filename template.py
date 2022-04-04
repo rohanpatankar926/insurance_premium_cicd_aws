619e78a344c3326be5a1a4c84f1c12f78121213c
Nix-code <nishantbanjade07@gmail.com>
2022-04-04 10:26:10 +0530

    add the directories and the files for the project

---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+# os.path helps to manage the directories easily irrespective of the operating system 
+
+dirs = [
+    os.path.join('data','raw'),
+    os.path.join('data','processes'),
+    "notebooks",
+    "saved_models",
+    "src",
+    "webapp",
+    "report",
+]
+for dir in dirs:
+    """ exist_ok checks if the folder is already created or not
+     if the dir is not created before, it will create new dir
+     else it will ignore"""
+    os.makedirs(dir, exist_ok = True)
+    with open(os.path.join(dir,'.gitkeep'),'w') as f:
+        pass
+
+# files for staging and pipeline
+
+files = [
+    'dvc.yaml',
+    'parameters.yaml',
+    '.gitignore',
+    os.path.join('src','__init__.py')
+]
+
+# __init__.py ensures that src file is the python package
+
+for file in files:
+    with open(file,'w') as f:
+        pass
