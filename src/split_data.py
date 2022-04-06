d8233809fee67131736b9b5c943e568a923415dd
Nix-code <nishantbanjade07@gmail.com>
2022-04-06 10:46:32 +0530

    label encode to the categorical features

--- src/split_data.py
+++ src/split_data.py
@@ -6,6 +6,8 @@ import argparse
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from get_data import read_params
+from sklearn.preprocessing import LabelEncoder
+
 
 def split_and_save(config_path):
     config=read_params(config_path)
@@ -16,11 +18,17 @@ def split_and_save(config_path):
     random_state = config["base"]["random_state"]
     
     df = pd.read_csv(raw_data_path, sep=",")
+    le = LabelEncoder()
+    df['sex'] = le.fit_transform(df['sex'])
+    df['smoker'] = le.fit_transform(df['smoker'])
+    df['region'] = le.fit_transform(df['region'])
+
     train, test = train_test_split(
         df, 
         test_size=split_ratio, 
         random_state=random_state
         )
+    
     train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
     test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
 
